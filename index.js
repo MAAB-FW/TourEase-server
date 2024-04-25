df3231538c52f1786c0de0a4273eb28de611f410
maab-fw <maabfw@proton.me>
2024-04-25 21:38:22 +0600

    mongodb connection string added+setup testing for live

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const { MongoClient, ServerApiVersion } = require("mongodb")
 require("dotenv").config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,6 +8,38 @@ const port = process.env.PORT || 5000
 // middleware
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkufpua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+
+// const uri = "mongodb://localhost:27017"
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    },
+})
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect()
+
+        app.get("/tourists-spots", (req, res) => {
+            res.send("testing live server")
+        })
+        
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 })
+        console.log("Pinged your deployment. You successfully connected to MongoDB!")
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 app.get("/", (req, res) => {
     res.send("server is running")
