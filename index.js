9019c1c6ecf4d819bb63e1b59c8777b6c1e99583
maab-fw <maabfw@proton.me>
2024-04-28 18:00:47 +0600

    live mongodb uri deployed

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkufpua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wkufpua.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const uri = "mongodb://localhost:27017"
+// const uri = "mongodb://localhost:27017"
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
